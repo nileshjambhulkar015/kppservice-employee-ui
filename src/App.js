b1258280742f2b636fd34174a9dd0124393411d5
nileshjambhulkar015 <nileshjambhulkar@gmail.com>
2023-11-18 19:10:12 +0530

    employee api added

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React from "react";
 import {Route, BrowserRouter as Router, Link, Switch} from 'react-router-dom';
 import EmployeeKppComponent from "./components/EmployeeKppsComponent/EmployeeKppComponent";
-
+import ChangePasswordComponent from "./components/ChangePasswordComponent/ChangePasswordComponent";
+import UpdateHodProfileComponent from "./components/UpdateHodProfileComponent/UpdateHodProfileComponent";
 function App() {
   return (
     <Router>
@@ -12,10 +13,10 @@ function App() {
         </div>
         <ul className="nav navbar-nav">
           <li className="active"><a href="#">Home</a></li>       
-          <li><Link to="/employeekpp">Employee KPP</Link></li>
+          <li><Link to="/employeekpp">Add KPP</Link></li>
           <li><Link to="/employeekpp">View KPP</Link></li>    
-          <li><Link to="/employeekpp">Update Profile</Link></li>  
-          <li><Link to="/employeekpp">Change Password</Link></li>         
+          <li><Link to="/updateHodProfile">View Profile</Link></li>  
+            <li><Link to="/changePassword">Change Password</Link></li>          
         </ul>
         <ul className="nav navbar-nav navbar-right">
           <li><a href="#"><span className="glyphicon glyphicon-user"></span> e1234</a></li>
@@ -27,6 +28,8 @@ function App() {
       
    
       <Route exact path="/employeekpp" component={EmployeeKppComponent}></Route>
+      <Route exact path="/updateHodProfile" component={UpdateHodProfileComponent}></Route>
+        <Route exact path="/changePassword" component={ChangePasswordComponent}></Route>
     </Switch>
   </Router>    
   );
--- src/services/EmployeeKppsService.js
+++ src/services/EmployeeKppsService.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL="http://localhost:9091/employee-key-perform-parameter?roleId=1&deptId=1&desigId=1&statusCdEnum=A";
+const BASE_URL="http://localhost:9091/hod-approval/employee-kpp?empId=1&statusCd=A";
 
 class EmployeeKppsService{
 
