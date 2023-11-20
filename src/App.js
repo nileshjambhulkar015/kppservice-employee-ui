5e2adc94df0605047c86ca70c621f8a0e6af8777
nileshjambhulkar015 <nileshjambhulkar@gmail.com>
2023-11-20 09:19:32 +0530

    routes added

--- src/App.js
+++ src/App.js
@@ -9,10 +9,10 @@ function App() {
     <nav className="navbar navbar-inverse">
       <div className="container-fluid">
         <div className="navbar-header">
-          <a className="navbar-brand" href="#">FutureBizops</a>
+          <a className="navbar-brand" href="http://localhost:3008">FutureBizops</a>
         </div>
         <ul className="nav navbar-nav">
-          <li className="active"><a href="#">Home</a></li>       
+          
           <li><Link to="/employeekpp">Add KPP</Link></li>
           <li><Link to="/employeekpp">View KPP</Link></li>    
           <li><Link to="/updateHodProfile">View Profile</Link></li>  
@@ -20,13 +20,13 @@ function App() {
         </ul>
         <ul className="nav navbar-nav navbar-right">
           <li><a href="#"><span className="glyphicon glyphicon-user"></span> e1234</a></li>
-          <li><a href="#"><span className="glyphicon glyphicon-log-in"></span> Login</a></li>
+          <li><a href="http://localhost:3008">Logout</a></li>
         </ul>
       </div>
     </nav>
     <Routes>
       
-   
+    <Route exact path="/" element={<EmployeeKppComponent />}></Route>
       <Route exact path="/employeekpp" element={<EmployeeKppComponent />}></Route>
       <Route exact path="/updateHodProfile" element={<UpdateHodProfileComponent />}></Route>
         <Route exact path="/changePassword" element={<ChangePasswordComponent />}></Route>
